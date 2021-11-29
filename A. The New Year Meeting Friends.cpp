0c1a0b480c7da81cd5879cca87f63309df5fce14
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-11-29 18:13:55 +0100

    1

---
+++ A. The New Year Meeting Friends.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x1,x2,x3,distance=0;
+    cin>>x1>>x2>>x3;
+    int tab[3] = {x1,x2,x3};
+
+    sort(tab,tab+3);
+    x1 = tab[0]; x2 = tab[1]; x3 = tab[2];
+
+    distance = x2-x1;
+    distance+=x3-x2;
+
+    cout<<distance;
+
+    return 0;
+}
