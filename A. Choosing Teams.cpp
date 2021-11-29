6e314948ab2eaa24bbe0e185b11c404cf057cd5e
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-11-29 19:17:19 +0100

    A. Choosing Teams

---
+++ A. Choosing Teams.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,k,team=0;
+    cin>>n>>k;
+    int tab[n];
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>tab[i];
+    }
+    
+    sort(tab,tab+n);
+    int s = n/3,x=-1;
+
+    for(int i=0; i<s; i++)
+    {
+        x+=3;
+        if(tab[x-2]+k<=5&&tab[x-1]+k<=5&&tab[x]+k<=5) {team++;}
+    }
+    cout<<team;
+
+    return 0;
+}
