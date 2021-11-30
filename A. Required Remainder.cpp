e6f34aa41d99de51e522225c4115e73bc8dcf279
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-11-30 19:21:11 +0100

    A. Required Remainder

---
+++ A. Required Remainder.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        int x,y,n;
+        cin>>x>>y>>n;
+
+        int pom = n/x;
+        int result = x * pom +y;
+
+        if(result>n) {result-=x;}
+        cout<<result<<endl;
+    }
+    
+    return 0;
+}
