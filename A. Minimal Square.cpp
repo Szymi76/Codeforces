3759dbac7be2d09548ae2374a9d51bf39a74a460
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-11-30 21:03:57 +0100

    A. Minimal Square

---
+++ A. Minimal Square.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        if(a>b) 
+        {
+            if(a>2*b) {cout<<a*a<<endl;}
+            else {cout<<(2*b)*(2*b)<<endl;}
+        }
+        else
+        {
+            if(b>2*a) {cout<<b*b<<endl;}
+            else {cout<<(2*a)*(2*a)<<endl;}
+        }
+    }
+        
+    return 0;
+}
