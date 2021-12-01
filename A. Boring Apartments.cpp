169de0eb4645f98529c718bf0eca0911d94054e9
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-01 18:55:04 +0100

    A. Boring Apartments

---
+++ A. Boring Apartments.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {   
+        cin>>n;
+        int x=1,num=0,sum=0;
+        
+        while(num!=n)
+        {
+            string ciag;
+            num=0;
+            
+            for(int j=1; j<5; j++)
+            {
+                string pom = to_string(x);
+                ciag+=pom;
+                istringstream(ciag) >> num;
+                sum+=j;
+                if(num==n) {break;} 
+            }
+            x++;
+        }
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
