b75705c64261476bf82e5621eefb3563d953ca00
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-01 19:35:51 +0100

    A. Soft Drinking

---
+++ A. Soft Drinking.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int mil = (k*l)/nl;
+    int limes = c*d;
+    int salt = p/np;
+
+    int tab[3] = {mil,limes,salt};
+    sort(tab,tab+3);
+    cout<<tab[0]/n;
+
+    return 0;
+}
