43f474bae4fd073a022a8a58b22ea2260066b00d
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-03 23:23:45 +0100

    B. Restore the Permutation by Merger

---
+++ B. Restore the Permutation by Merger.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {   
+        cin>>n;
+        int tab[2*n];
+
+        for(int j=0; j<2*n; j++)
+        {
+            cin>>tab[j];
+        }        
+
+        for(int j=0; j<2*n; j++)
+        {
+            for(int k=0; k<2*n; k++)
+            {
+                if(j!=k)
+                {
+                    if(tab[k]==tab[j]) {tab[k]=0;}
+                }
+            }
+        }
+
+        for(int j=0; j<2*n; j++)
+        {
+            if(tab[j]!=0)
+            {
+                cout<<tab[j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
