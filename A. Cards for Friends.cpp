cb66a7223d014fb74c2c5651bb89f05b07e096c0
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-03 21:33:27 +0100

    A. Cards for Friends

---
+++ A. Cards for Friends.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ll t,w,h,n;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        cin>>w>>h>>n;
+        ll sum=0,sum1=w*h;;
+
+        while((w%2==0||h%2==0)&&(w!=1||h!=1))
+        {
+            if(w%2==0&&w!=1) {w = w/2;} 
+            else if(h%2==0&&h!=1) {h = h/2;};
+        }
+        for(int j=0; j<n; j++)
+        {   
+            sum+=w*h;
+        }
+        if(sum<=sum1) {cout<<"YES"<<endl;}
+        else {cout<<"NO"<<endl;}
+    }
+
+    return 0;
+}
