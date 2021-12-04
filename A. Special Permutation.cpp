696c9a9d058b951f716a824756a723c02c1c5c70
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-04 22:30:07 +0100

    A. Special Permutation

---
+++ A. Special Permutation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t,n;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        cin>>n;
+        int tab[n];
+
+        for(int j=1; j<=n; j++)
+        {
+            tab[j] = j;
+        }
+
+        for(int j=1; j<n; j++)
+        {
+            swap(tab[j],tab[j+1]);
+        }
+
+        for(int j=1; j<=n; j++)
+        {
+            cout<<tab[j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
