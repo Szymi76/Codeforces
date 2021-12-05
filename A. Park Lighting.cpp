c1a72d3d3e05ce26ecd066e458dc8429849f6239
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-05 01:32:30 +0100

    A. Park Lighting

---
+++ A. Park Lighting.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t,n,m;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        cin>>n>>m;
+
+        if(n*m%2==0) {cout<<(n*m)/2<<endl;}
+        else {cout<<(n*m)/2+1<<endl;}
+    }
+
+    return 0;
+}
