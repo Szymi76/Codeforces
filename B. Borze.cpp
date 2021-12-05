74f62adfb326f314183e8dd70c6e08feccba6618
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-05 01:42:43 +0100

    B. Borze

---
+++ B. Borze.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    string ciag;
+    cin>>ciag;
+    int n=0;
+
+    while(n<ciag.length())
+    {
+        if(ciag[n]=='.') {cout<<"0"; n++;}
+        else
+        {
+            if(ciag[n+1]=='.') {cout<<"1"; n+=2;}
+            else {cout<<"2"; n+=2;}
+        }
+    }
+
+    return 0;
+}
