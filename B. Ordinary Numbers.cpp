26c588f2c94fddf5bfc3873ffd9e9ab342406cd6
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-05 02:59:36 +0100

    B. Ordinary Numbers

---
+++ B. Ordinary Numbers.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        int n;
+        cin>>n;
+        if(n>0&&n<10) {cout<<n<<endl;}
+        else
+        {
+            int s=1;
+            int num = 11;
+            int result = 0;
+            int x=11;
+            string pom1 = "1";
+
+            while(x<=n)
+            {
+                if(s==9)
+                {
+                    x+=num+1;
+                    string pom = to_string(num);
+                    pom = pom + pom1;
+                    istringstream(pom) >> num;
+                    s = 0;
+                }
+                else
+                {
+                    x+=num;
+                }
+            s++;
+            result++;
+            }
+            cout<<result+9<<endl;
+        }
+    }
+
+    return 0;
+}
