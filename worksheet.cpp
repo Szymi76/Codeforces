e6ffb2267cd8017e055d10f811ed50ea20bfeab6
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2021-12-20 21:13:18 +0100

    c++

--- worksheet.cpp
+++ worksheet.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main()
 {
-    int t,n;
-    cin>>t;
-
-    for(int i=0; i<t; i++)
-    {
-        cin>>n;
-
-        for(int i=0; i<n; i++)
-        {
-            cout<<"1 ";
-        }
-        cout<<endl;
-    }
+    int a,b;
+    cin>>a>>b;
     
+    int pom;
+
+    while(b!=0)
+    {
+        pom = b;
+        b = a%b;
+        a = pom;
+    }
+
+    cout<<a;
+
     return 0;
 }
